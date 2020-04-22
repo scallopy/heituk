35084f225038aaf752aeb9d34a2a35ad36a921a8
scallopy <scallopy@abv.bg>
2020-04-22 08:46:34 +0300

    Create Views for users_list

--- django_react_proj/django_react_proj/urls.py
+++ django_react_proj/django_react_proj/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from growvick import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^api/users/$', views.users_list),
+    re_path(r'^api/users/(?P<pk>[0-9]+)$', views.users_detail),
 ]
